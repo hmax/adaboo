b3ddf355d5e4e93b2e833e7266d741c936e972b8
Max Khardin <mkhardin@yandex-team.ru>
2014-04-01 21:58:04 +0400

    Add move constructor to images

--- src/image.hpp
+++ src/image.hpp
@@ -8,9 +8,11 @@ template <typename T> class Image
 		size_t width, height;
 	public:
 		using value_type = T;
-		explicit Image(void);
+		explicit Image(void) : data(nullptr), width(0), height(0) {};
 		Image(const Image<T>& img);
+		Image(Image&& other);
 		Image<T>& operator= (const Image<T>& rhs);
+		Image<T>& operator= (const Image<T>&& rhs);
 		Image(size_t width, size_t height, T* data);
 		T& at(size_t x, size_t y) const;
 		~Image(void);
--- src/image_impl.hpp
+++ src/image_impl.hpp
@@ -1,15 +1,11 @@
 #include "image.hpp"
 #include <cstring>
-template <typename T> Image<T>::Image(void) : data(nullptr), width(0), height(0) {}
 
-
-template <typename T> Image<T>::Image(size_t width, size_t height, T* data) : data(new T[width * height]), width(width), height(height)
-{
+template <typename T> Image<T>::Image(size_t width, size_t height, T* data) : data(new T[width * height]), width(width), height(height){
 	memcpy(this->data, data, width*height);
 }
 
-template <typename T> Image<T>::Image(const Image<T>& img) : data(new T[img.width * img.height]), width(img.width), height(img.height)
-{
+template <typename T> Image<T>::Image(const Image<T>& img) : data(new T[img.width * img.height]), width(img.width), height(img.height){
 	memcpy(this->data, img.data, width*height);
 }
 
@@ -17,7 +13,6 @@ template<typename T> Image<T>& Image<T>::operator=( const Image<T>& rhs ) {
 	if (this != &rhs){
 		T* new_data = new T[rhs.width * rhs.height];
 		memcpy(this->data, rhs.data, rhs.width*rhs.height);
-
 		delete[] this->data;
 
 		this->width = rhs.width;
@@ -26,12 +21,32 @@ template<typename T> Image<T>& Image<T>::operator=( const Image<T>& rhs ) {
 	};
 	return *this;
 }
-template <typename T> T& Image<T>::at(size_t x, size_t y) const
-{
+
+template<typename T> Image<T>::Image(Image<T>&& other) : data(other.data), width(other.width), height(other.height){ 
+	other.data = nullptr; 
+	other.width = 0;
+	other.height = 0;
+}
+
+template<typename T> Image<T>& Image<T>::operator= (const Image<T>&& rhs){
+	if (this != &rhs){
+		this->data = rhs.data;
+		this->width = rhs.width;
+		this->height = rhs.height;
+
+		rhs.data = nullptr;
+		rhs.width = 0;
+		rhs.height = 0;
+	}
+	return *this;
+}
+
+
+template<typename T> T& Image<T>::at(size_t x, size_t y) const{
 	return this->data[this->width * y + x];
 }
 
-template <typename T> Image<T>::~Image(void)
-{
+template <typename T> Image<T>::~Image(void){
+	// Should I check for nullptr? Using default
 	delete[] data;
 }
