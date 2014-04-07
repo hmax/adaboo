abfc2205551e1c7e4714fcf4eb8cf1b92ed96451
Max Khardin <max.khardin@gmail.com>
2014-04-07 22:32:00 +0400

    Throw out unused sample_weights, add little optimization
    Use xor instead of branching for calculation of classifier_error

--- src/AdaBooster_impl.hpp
+++ src/AdaBooster_impl.hpp
@@ -11,7 +11,7 @@ template <typename T> AdaBooster<T>::AdaBooster() : classifiers(), errors(){
 }
 
 template <typename T> void AdaBooster<T>::train(vector<std::shared_ptr<AbstractWeakClassifier<T>>> weak_classifiers, TrainSet<T> train_set, int number_of_classifiers){
-	vector<vector<double> > weights(number_of_classifiers + 1, vector<double>(train_set.size()));
+	vector<double> sample_weights(train_set.size());
 	size_t female_samples = 0;
 	size_t male_samples = 0;
 
@@ -22,25 +22,21 @@ template <typename T> void AdaBooster<T>::train(vector<std::shared_ptr<AbstractW
 			female_samples++;
 	}
 
-	for(auto w : weights)
-		w.reserve(train_set.size());
-
-	for(auto i = 0; i < weights.at(0).size(); ++i){
+	for (auto i = 0; i < sample_weights.size(); ++i){
 		if(train_set.at(i).second == 0)
-			weights.at(0).at(i) = 0.5/male_samples;
+			sample_weights.at(i) = 0.5 / male_samples;
 		else
-			weights.at(0).at(i) = 0.5/female_samples;
+			sample_weights.at(i) = 0.5 / female_samples;
 	}
 
 
 	//Actual AdaBoost adaptation from paper
 	for(auto classifier_number = 0; classifier_number < number_of_classifiers; ++classifier_number){
-		auto iteration_weights = weights.at(classifier_number);
 		auto weight_norm = 0.0;
 
-		weight_norm = std::accumulate(iteration_weights.begin(), iteration_weights.end(), 0.0);
+		weight_norm = std::accumulate(sample_weights.begin(), sample_weights.end(), 0.0);
 		
-		for(auto& weight : iteration_weights){
+		for (auto& weight : sample_weights){
 			weight = weight / weight_norm;
 		}
 
@@ -50,13 +46,13 @@ template <typename T> void AdaBooster<T>::train(vector<std::shared_ptr<AbstractW
 		for(auto weak_classifier_it = weak_classifiers.cbegin(); weak_classifier_it != weak_classifiers.cend(); ++weak_classifier_it){
 			auto classifier_error = 0.0;
 			auto sample = train_set.cbegin();
-			auto sample_weight = iteration_weights.cbegin();
-			
-			for(; sample != train_set.cend() && sample_weight != iteration_weights.cend(); ++sample, ++sample_weight){
+			auto sample_weight = sample_weights.cbegin();
+
+			for (; sample != train_set.cend() && sample_weight != sample_weights.cend(); ++sample, ++sample_weight){
 				int classsification_result = (*weak_classifier_it)->classify(sample->first);
-				if(classsification_result != sample->second)
-					classifier_error += *sample_weight;
+				classifier_error += *sample_weight * (classsification_result ^ sample->second);  // Since classes are 0 and 1 we can use xor and multiplication to not confuse branch prediction here
 			}
+
 			if(classifier_error < error){
 				classifier = *weak_classifier_it;
 				error = classifier_error;
@@ -68,18 +64,12 @@ template <typename T> void AdaBooster<T>::train(vector<std::shared_ptr<AbstractW
 		
 		// Reweight samples
 		auto b = error / (1 - error);
-		auto iteration_weight = iteration_weights.cbegin();
-		auto next_iteration_weight = weights.at(classifier_number + 1).begin();
+		auto sample_weight = sample_weights.begin();
 		auto sample = train_set.cbegin();
 
-		for (; sample != train_set.cend() && iteration_weight != iteration_weights.cend() && next_iteration_weight != weights.at(classifier_number + 1).end();
-			++sample, ++iteration_weight, ++next_iteration_weight){
-			if(classifier->classify(sample->first) == sample->second){
-				*next_iteration_weight = (*iteration_weight) * b;
-			}
-			else{
-				*next_iteration_weight = *iteration_weight;
-			}
+		for (; sample != train_set.cend() && sample_weight != sample_weights.end();	++sample, ++sample_weight){
+			if(classifier->classify(sample->first) == sample->second)
+				*sample_weight = (*sample_weight) * b;
 		}
 	}
 }
