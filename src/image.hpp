b207193973b4f927a2da05c417aac9331f883a69
Max Khardin <max.khardin@gmail.com>
2014-05-26 00:46:36 +0400

    Some initial shuffling of stuff around, preparing for viola-jones goodness

--- src/image.hpp
+++ src/image.hpp
@@ -5,17 +5,27 @@
 template <typename T> class Image
 {
 		T* data;
-		size_t width, height;
+		size_t m_width, m_height;
 	public:
 		using value_type = T;
-		explicit Image(void) : data(nullptr), width(0), height(0) {};
-		Image(const Image<T>& img);
+		
+		Image() = delete;  // This constructor makes no sense for now
+
+		template<typename F> explicit Image(const Image<F>& img);
 		Image(Image&& other);
+		
 		Image<T>& operator= (const Image<T>& rhs);
 		Image<T>& operator= (const Image<T>&& rhs);
+
 		Image(size_t width, size_t height, T* data);
-		T& at(size_t x, size_t y) const;
+		
+		T& at(size_t x, size_t y);
+		T at(size_t x, size_t y) const;
+		size_t width() const { return this->m_width; };
+		size_t height() const { return this->m_height; };
+
 		~Image(void);
+
 };
 
 #include "image_impl.hpp"
--- src/image_impl.hpp
+++ src/image_impl.hpp
@@ -1,49 +1,54 @@
 #include "image.hpp"
 #include <cstring>
 
-template <typename T> Image<T>::Image(size_t width, size_t height, T* data) : data(new T[width * height]), width(width), height(height){
+template <typename T> Image<T>::Image(size_t width, size_t height, T* data) : data(new T[width * height]), m_width(width), m_height(height){
 	memcpy(this->data, data, width*height);
 }
 
-template <typename T> Image<T>::Image(const Image<T>& img) : data(new T[img.width * img.height]), width(img.width), height(img.height){
-	memcpy(this->data, img.data, width*height);
+template <typename T>
+template <typename F> Image<T>::Image(const Image<F>& img) : data(new T[img.width() * img.height()]), m_width(img.width()), m_height(img.height()){
+	memcpy(this->data, img.data, m_width*m_height);
 }
 
 template<typename T> Image<T>& Image<T>::operator=( const Image<T>& rhs ) {
 	if (this != &rhs){
-		T* new_data = new T[rhs.width * rhs.height];
-		memcpy(this->data, rhs.data, rhs.width*rhs.height);
+		T* new_data = new T[rhs.m_width * rhs.m_height];
+		memcpy(this->data, rhs.data, rhs.m_width*rhs.m_height);
 		delete[] this->data;
 
-		this->width = rhs.width;
-		this->height = rhs.height;
+		this->m_width = rhs.m_width;
+		this->m_height = rhs.m_height;
 		this->data = new_data;
 	};
 	return *this;
 }
 
-template<typename T> Image<T>::Image(Image<T>&& other) : data(other.data), width(other.width), height(other.height){ 
+template<typename T> Image<T>::Image(Image<T>&& other) : data(other.data), m_width(other.m_width), m_height(other.m_height){
 	other.data = nullptr; 
-	other.width = 0;
-	other.height = 0;
+	other.m_width = 0;
+	other.m_height = 0;
 }
 
 template<typename T> Image<T>& Image<T>::operator= (const Image<T>&& rhs){
 	if (this != &rhs){
 		this->data = rhs.data;
-		this->width = rhs.width;
-		this->height = rhs.height;
+		this->m_width = rhs.m_width;
+		this->m_height = rhs.m_height;
 
 		rhs.data = nullptr;
-		rhs.width = 0;
-		rhs.height = 0;
+		rhs.m_width = 0;
+		rhs.m_height = 0;
 	}
 	return *this;
 }
 
 
-template<typename T> T& Image<T>::at(size_t x, size_t y) const{
-	return this->data[this->width * y + x];
+template<typename T> T& Image<T>::at(size_t x, size_t y){
+	return this->data[this->m_width * y + x];
+}
+
+template<typename T> T Image<T>::at(size_t x, size_t y) const{
+	return this->data[this->m_width * y + x];
 }
 
 template <typename T> Image<T>::~Image(void){
